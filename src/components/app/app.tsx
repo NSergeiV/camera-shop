be2c76a7d4e39b6069e230bac921a3fe599c5f73
NSergeiV <ttniseva@yandex.ru>
2023-05-25 19:27:20 +0300

    Собрал проект, реализовал три страницы и маршрут

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,41 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+import { AppRoute } from '../../const/const';
+
+import Catalog from '../../pages/catalog/catalog';
+import Layout from '../layout/layout';
+import Product from '../../pages/product/product';
+import Basket from '../../pages/basket/basket';
+import NoPage from '../../pages/no-page/no-page';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Catalog} element={<Layout />}>
+            <Route
+              index
+              element={<Catalog />}
+            />
+            <Route
+              path={AppRoute.Product}
+              element={<Product />}
+            />
+            <Route
+              path={AppRoute.Basket}
+              element={<Basket />}
+            />
+          </Route>
+          <Route
+            path='*'
+            element={<NoPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
 export default App;
